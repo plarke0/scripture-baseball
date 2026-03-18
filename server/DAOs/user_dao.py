af6baf1ab2aa621535e9910d122186d158b117fa
plarke0 <plarke@byu.edu>
2026-03-18 00:39:25 -0600

    Implement get_user

--- server/DAOs/user_dao.py
+++ server/DAOs/user_dao.py
@@ -6,8 +6,15 @@ class UserDAO:
     def __init__(self) -> None:
         self.db_manager = DatabaseManager()
         
-    def get_user(self, username: str) -> UserData:
-        ...
+    def get_user(self, username: str) -> UserData | None:
+        sql = "SELECT username, email, password FROM users WHERE username = %s"
+        val = (username, )
+        
+        user = self.db_manager.select_one(sql, val)
+        if user is None:
+            return
+        
+        return UserData(user[0], user[1], user[2])
         
     def insert_user(self, user_data: UserData) -> None:
         ...
