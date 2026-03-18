5c5247778f854db5c237a0b5f494d45df1b348f7
plarke0 <plarke@byu.edu>
2026-03-17 23:52:05 -0600

    Implement get_verse

--- server/server.py
+++ server/server.py
@@ -9,10 +9,20 @@ from shared.response_classes import VerseResponse, RegisterResponse, LoginRespon
 class Server:
     
     def __init__(self) -> None:
-        ...
+        self.auth_dao = AuthDAO()
+        self.user_dao = UserDAO()
+        self.score_dao = ScoreDAO()
     
     def get_verse(self, auth_token: str, verse_request: VerseRequest) -> VerseResponse:
-        ...
+        self.check_auth(auth_token)
+        
+        volume: str = verse_request.volume
+        book: str = verse_request.book
+        chapter_number: int = verse_request.chapter
+        verse_number: int = verse_request.verse
+        
+        chapter: list[str] = APIHandlers.get_chapter(volume, book, chapter_number)
+        return VerseResponse(chapter, chapter[verse_number-1])
         
     def register_user(self, register_request: RegisterRequest) -> RegisterResponse:
         ...
@@ -31,3 +41,8 @@ class Server:
         
     def get_top(self, auth_token: str, top_scores_request: TopScoresRequest) -> TopScoresResponse:
         ...
+        
+    def check_auth(self, auth_token: str) -> None:
+        auth_data: AuthData | None = self.auth_dao.get_auth(auth_token)
+        if auth_data is None:
+            raise ValueError("Invalid auth token")
