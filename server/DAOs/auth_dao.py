f88b2329c0fa82a5ca70d22f50474d9fcd201b02
plarke0 <plarke@byu.edu>
2026-03-17 23:25:37 -0600

    Implement get_auth

--- server/DAOs/auth_dao.py
+++ server/DAOs/auth_dao.py
@@ -1,12 +1,24 @@
+from server.database_manager import DatabaseManager
 from shared.data_classes import AuthData
 
 class AuthDAO:
     
     def __init__(self) -> None:
-        ...
+        self.db_manager = DatabaseManager()
         
-    def get_auth(self, auth_token: str) -> AuthData:
-        ...
+    def get_auth(self, auth_token: str) -> AuthData | None:
+        cursor = self.db_manager.get_cursor()
+        cursor.execute(f"SELECT user_id, auth_token FROM auths WHERE auth_token={auth_token}")
+        query_list: list[tuple] = cursor.fetchall() # type: ignore
+        
+        if len(query_list) == 0:
+            return
+        
+        if len(query_list) > 1:
+            raise ValueError("Found more than one entry for the given auth")
+        
+        return AuthData(query_list[0][0], query_list[0][1])
+        
         
     def insert_auth(self, auth_data: AuthData) -> None:
         ...
